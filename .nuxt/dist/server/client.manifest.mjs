d8ec34065242798e1fdd90900b35f4b33eaff94e
Will Smithee <wasmithee@gmail.com>
2022-06-09 12:38:49 -0600

    First Commit

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,10 @@
+export default {
+  "node_modules/nuxt/dist/app/entry.mjs": {
+    "file": "entry-7859aa13.mjs",
+    "src": "node_modules/nuxt/dist/app/entry.mjs",
+    "isEntry": true,
+    "css": [
+      "entry.fec640a4.css"
+    ]
+  }
+}
